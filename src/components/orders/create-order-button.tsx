276c74eeea2e6e1432c22b3de2864f2315b18644
thomasbaach <thomas.baach@gmail.com>
2026-03-15 13:09:24 +0100

    Gelber butten

--- src/components/orders/create-order-button.tsx
+++ src/components/orders/create-order-button.tsx
@@ -7,7 +7,7 @@ import { Button } from "@/components/ui/button";
 export function CreateOrderButton({ contacts: _ }: { contacts?: unknown[] }) {
   const router = useRouter();
   return (
-    <Button size="sm" onClick={() => router.push("/auftraege/neu")}>
+    <Button size="sm" className="bg-yellow-400 hover:bg-yellow-500 text-black" onClick={() => router.push("/auftraege/neu")}>
       <Plus className="h-4 w-4 mr-1" />
       Neuer Auftrag
     </Button>
